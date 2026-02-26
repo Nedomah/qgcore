41b836d354ee742ed76054cd8dcadf42a1ece2c5
Nedomah <leroux.philip@gmail.com>
2026-02-26 21:22:02 +0200

    Test connection manager

--- public/index.php
+++ public/index.php
@@ -3,6 +3,13 @@
 require __DIR__ . '/../connections/conman.php';
 require __DIR__ . '/../devtools/tools.php';
 
-echo phpinfo();
+$pdo = ConMan::get();
+
+// Plain query
+$stmt = $pdo->prepare('SELECT * FROM Connections');
+$stmt->execute();
+$data = $stmt->fetchAll();
+
+echo json_encode($data)
 
 ?>
